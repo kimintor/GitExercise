f906436456e25efd524cbb2ed1060420df1af479
Alex Dulghier <92308490+kimintor@users.noreply.github.com>
2023-02-11 12:50:27 +0100

    second commit

---
+++ src/main/java/org/example/FirstBranchClass.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class FirstBranchClass {
+    private String here;
+
+    public FirstBranchClass(String here) {
+        this.here = here;
+    }
+
+    public String getHere() {
+        return here;
+    }
+
+    public void setHere(String here) {
+        this.here = here;
+    }
+}
